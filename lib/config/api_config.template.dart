57eeec1d268478a2e1d06ba2c08abbc719188d48
Parthsaarthie Sharma <parthsaarthiesharma1909@yahoo.com>
2025-07-21 14:34:37 +0530

    COMMIT

--- lib/config/api_config.template.dart
+++ lib/config/api_config.template.dart
@@ -1,4 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'YOUR_API_BASE_URL';
-  static const String cryptoEndpoint = 'YOUR_CRYPTO_ENDPOINT';
+  static const String baseUrl = 'http://161.97.157.232:8085/Bitcoin/resources';
+  static const String cryptoEndpoint = '/getBitcoinCryptoList?size=0&currency=USD';
+  static const String cryptoLoserEndpoint = '/getBitcoinCryptoListLoser?size=0&currency=USD';
 } 
--- lib/services/crypto_api_service.dart
+++ lib/services/crypto_api_service.dart
@@ -6,7 +6,7 @@ import '../models/crypto_coin.dart';
 class CryptoApiService {
   Future<List<CryptoCoin>> getCryptoList({String currency = 'USD', int size = 0}) async {
     try {
-      final url = Uri.parse('${ApiConfig.baseUrl}${ApiConfig.cryptoEndpoint}?size=$size&currency=$currency');
+      final url = Uri.parse('${ApiConfig.baseUrl}${ApiConfig.cryptoEndpoint}');
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
@@ -20,6 +20,25 @@ class CryptoApiService {
       throw Exception('Failed to load crypto data');
     } catch (e) {
       throw Exception('Error fetching crypto data: $e');
+    }
+  }
+
+  Future<List<CryptoCoin>> getCryptoLoserList({String currency = 'USD', int size = 0}) async {
+    try {
+      final url = Uri.parse('${ApiConfig.baseUrl}${ApiConfig.cryptoLoserEndpoint}');
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+        if (data['status'] == 'success' && data['data'] != null) {
+          return (data['data'] as List)
+              .map((coin) => CryptoCoin.fromJson(coin))
+              .toList();
+        }
+      }
+      throw Exception('Failed to load crypto loser data');
+    } catch (e) {
+      throw Exception('Error fetching crypto loser data: $e');
     }
   }
 } 
